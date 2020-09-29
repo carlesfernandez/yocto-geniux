941e31865c35b1ec7c3e682d5d1cae8d08cd6d7e
Carles Fernandez <carles.fernandez@gmail.com>
2020-09-30 01:11:04 +0200

    First commit

---
+++ Dockerfile
@@ -0,0 +1,112 @@
+# SPDX-FileCopyrightText: 2020, Carles Fernandez-Prades <carles.fernandez@cttc.es>
+# SPDX-License-Identifier: Apache-2.0
+#
+# Docker image to build Yocto images for GNSS-SDR.
+# Adapted from https://github.com/bstubert/dr-yocto, by Burkhard Stubert
+
+FROM ubuntu:18.04
+
+# Install all Linux packages required for Yocto builds, plus other packages used
+# in this file below, and in the interactive mode
+RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get -y install \
+    gawk wget git-core diffstat unzip texinfo gcc-multilib \
+    build-essential chrpath socat cpio python python3 python3-pip python3-pexpect \
+    xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev \
+    pylint3 xterm git-lfs locales sudo apt-transport-https ca-certificates curl \
+    gnupg-agent software-properties-common nano && apt-get clean && rm -rf /var/lib/apt/lists/*
+
+# By default, Ubuntu uses dash as an alias for sh. Dash does not support the source command
+# needed for setting up Yocto build environments. Use bash as an alias for sh.
+RUN which dash &> /dev/null && (\
+    echo "dash dash/sh boolean false" | debconf-set-selections && \
+    DEBIAN_FRONTEND=noninteractive dpkg-reconfigure dash) || \
+    echo "Skipping dash reconfigure (not applicable)"
+
+# Install docker
+RUN curl -fsSL https://download.docker.com/linux/ubuntu/gpg | APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1 apt-key add -
+RUN add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable" && apt-get update && DEBIAN_FRONTEND=noninteractive \
+    apt-get -y install docker-ce docker-ce-cli containerd.io \
+    && apt-get clean && rm -rf /var/lib/apt/lists/*
+
+# Install the repo tool to handle git submodules (meta layers) comfortably.
+ADD https://storage.googleapis.com/git-repo-downloads/repo /usr/local/bin/
+RUN chmod 755 /usr/local/bin/repo
+
+ARG version=thud
+ARG manifest_date=latest
+ARG MACHINE=zedboard-zynq7
+ARG host_uid=1001
+ARG host_gid=1001
+
+# Set up a local mirror
+ENV LOCAL_MIRROR /source_mirror/sources/$version
+RUN mkdir -p $LOCAL_MIRROR
+
+# Set the locale to en_US.UTF-8, because the Yocto build fails without any locale set.
+RUN locale-gen en_US.UTF-8 && update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+ENV LANG en_US.UTF-8
+ENV LC_ALL en_US.UTF-8
+
+# Add user "geniux" to sudoers. Then, the user can install Linux packages in the container.
+ENV USER_NAME geniux
+RUN echo "${USER_NAME} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${USER_NAME} && \
+    chmod 0440 /etc/sudoers.d/${USER_NAME}
+
+# The running container writes all the build artefacts to a host directory (outside the container).
+# The container can only write files to host directories, if it uses the same user ID and
+# group ID owning the host directories. The host_uid and group_uid are passed to the docker build
+# command with the --build-arg option. By default, they are both 1001. The docker image creates
+# a group with host_gid and a user with host_uid and adds the user to the group. The symbolic
+# name of the group and user is geniux.
+RUN groupadd -g $host_gid $USER_NAME && useradd -g $host_gid -m -s /bin/bash -u $host_uid $USER_NAME
+
+# Perform the Yocto build as user geniux (not as root).
+# By default, docker runs as root. However, Yocto builds should not be run as root, but as a
+# normal user. Hence, we switch to the newly created user geniux.
+USER $USER_NAME
+ENV BUILD_INPUT_DIR /home/$USER_NAME/yocto/input
+ENV BUILD_OUTPUT_DIR /home/$USER_NAME/yocto/output
+RUN mkdir -p $BUILD_INPUT_DIR $BUILD_OUTPUT_DIR
+
+RUN sudo usermod -aG docker $USER_NAME && newgrp docker
+
+WORKDIR $BUILD_INPUT_DIR
+
+RUN if [ "$manifest_date" = "latest" ] ; then \
+    repo init -u git://github.com/carlesfernandez/oe-gnss-sdr-manifest.git -b $version ; \
+    else \
+    repo init -u git://github.com/carlesfernandez/oe-gnss-sdr-manifest.git -b refs/tags/$version-$manifest_date ; \
+    fi
+
+RUN repo sync
+ENV MACHINE=$MACHINE
+ENV TEMPLATECONF=$BUILD_INPUT_DIR/meta-gnss-sdr/conf
+
+CMD if [ "$host_git" = "1001" ]; then \
+    source ./oe-core/oe-init-build-env ./build ./bitbake && \
+    sudo service docker start && \
+    bitbake gnss-sdr-dev-image && \
+    bitbake -c populate_sdk gnss-sdr-dev-image && \
+    bitbake gnss-sdr-demo-image && \
+    bitbake gnss-sdr-dev-docker && \
+    sudo mv ./tmp-glibc/deploy/images /home/geniux/yocto/output/ && \
+    rm ./downloads/*.done && \
+    rm -rf ./downloads/git2 && \
+    sudo mv ./downloads /home/geniux/yocto/output/ && \
+    sudo mv ./tmp-glibc/deploy/sdk /home/geniux/yocto/output/ ; \
+      else \
+    source ./oe-core/oe-init-build-env ./build ./bitbake && \
+    echo "" | sudo -S service docker start && \
+    bitbake gnss-sdr-dev-image && \
+    bitbake -c populate_sdk gnss-sdr-dev-image && \
+    bitbake gnss-sdr-demo-image && \
+    bitbake gnss-sdr-dev-docker && \
+    mv ./tmp-glibc/deploy/images /home/geniux/yocto/output/ && \
+    rm ./downloads/*.done && \
+    rm -rf ./downloads/git2 && \
+    mv ./downloads /home/geniux/yocto/output/ && \
+    mv ./tmp-glibc/deploy/sdk /home/geniux/yocto/output/ ; \
+    fi
---
+++ geniux-builder.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+# A script to build and store Geniux releases
+# SPDX-FileCopyrightText: 2020, Carles Fernandez-Prades <carles.fernandez@cttc.es>
+# SPDX-License-Identifier: Apache-2.0
+
+display_usage() {
+	echo -e "This script builds and stores Geniux images."
+    echo -e "\nUsage:\n./geniux-builder.sh [version] [manifest] [machine]\n"
+    echo -e "Options:"
+    echo -e " version   Geniux version: rocko, sumo, thud, warrior, zeus, dunfell. Default: warrior"
+	echo -e "           Check available branches at https://github.com/carlesfernandez/meta-gnss-sdr"
+    echo -e " manifest  Geniux version manifest: 20.09, ..., latest. Default: latest"
+	echo -e "           Dated manifests available at https://github.com/carlesfernandez/oe-gnss-sdr-manifest/tags"
+    echo -e " machine   Specify your (list of) MACHINE here. By default, zedboard-zynq7 and raspberrypi3 are built.\n"
+    echo -e "Environment variables that affect behavior:"
+    echo -e " GENIUX_MIRROR_PATH          Base path to local mirror. Only used if set."
+    echo -e "                             e.g.: 'export GENIUX_MIRROR_PATH=/home/$USER/mirror'"
+	echo -e "                             The mirror is expected to be at '\$GENIUX_MIRROR_PATH/sources/\$version'"
+    echo -e " GENIUX_STORE_PATH           Path in which products will be stored. Only used if set."
+    echo -e "                             e.g.: 'export GENIUX_STORE_PATH=/home/$USER/geniux-releases'"
+    echo -e " GENIUX_STORE_REQUIRES_SUDO  If set, the script will ask for super-user privileges to write in the store."
+	echo -e "                             You will be asked only once at the beginning. The password will not be revealed."
+	echo -e "                             e.g.: 'export GENIUX_STORE_REQUIRES_SUDO=1'"
+}
+
+if [[ ( $1 == "--help") ||  $1 == "-h" ]]
+	then
+		display_usage
+		exit 0
+fi
+
+if [ $# -gt 3 ]
+    then
+        echo -e "Too much arguments!\n"
+    	display_usage
+    	exit 1
+fi
+
+GENIUX_VERSION=${1:-warrior}
+GENIUX_MANIFEST_DATE=${2:-latest}
+
+MIRROR_PATH=$GENIUX_MIRROR_PATH
+STORE_PATH=$GENIUX_STORE_PATH
+
+if [ $STORE_PATH ]
+    then
+        STORE_REQUIRES_SUDO=$GENIUX_STORE_REQUIRES_SUDO
+fi
+
+ListOfMachines="zedboard-zynq7 raspberrypi3"
+if [ $# -eq 3 ]
+    then
+        ListOfMachines="$3"
+fi
+
+echo -e "This script will build the Geniux distribution, version $GENIUX_VERSION-$GENIUX_MANIFEST_DATE, for machine(s): $ListOfMachines"
+if [ $STORE_PATH ]
+    then
+        echo -e "Products will be stored at $STORE_PATH/$GENIUX_VERSION/$GENIUX_MANIFEST_DATE"
+fi
+
+if [ $MIRROR_PATH ]
+    then
+        echo -e "The source files mirror will point to $MIRROR_PATH/sources/$GENIUX_VERSION\n"
+fi
+
+if [ $STORE_REQUIRES_SUDO ]
+    then
+        read -s -p "Enter Password for sudo: " sudoPW
+fi
+
+BASEDIR=$PWD
+
+mkdir -p $GENIUX_VERSION && cd $GENIUX_VERSION
+
+for machine in $ListOfMachines; do
+    echo "Building Geniux $GENIUX_VERSION-$GENIUX_MANIFEST_DATE for machine $machine..."
+    mkdir -p $GENIUX_VERSION-$machine && cd $GENIUX_VERSION-$machine && cp ../../Dockerfile ./
+    docker build --no-cache \
+	  --build-arg "version=$GENIUX_VERSION" \
+	  --build-arg "manifest_date=$GENIUX_MANIFEST_DATE" \
+	  --build-arg "MACHINE=$machine" \
+	  --build-arg "host_uid=$(id -u)" --build-arg "host_gid=$(id -g)" \
+	  --tag "geniux-$GENIUX_VERSION:$GENIUX_MANIFEST_DATE.$machine" .
+    mkdir -p output_$machine
+    if [ $MIRROR_PATH ]
+        then
+            docker run -it --rm -v $PWD/output_$machine:/home/geniux/yocto/output \
+			  -v $MIRROR_PATH/sources/$GENIUX_VERSION:/source_mirror/sources/$GENIUX_VERSION \
+			  --privileged=true \
+			  geniux-$GENIUX_VERSION:$GENIUX_MANIFEST_DATE.$machine
+        else
+			docker run -it --rm -v $PWD/output_$machine:/home/geniux/yocto/output \
+			  --privileged=true \
+			  geniux-$GENIUX_VERSION:$GENIUX_MANIFEST_DATE.$machine
+	fi
+	cd output_$machine/images
+	if [ $STORE_REQUIRES_SUDO ]
+	    then
+            echo $sudoPW | sudo -S zip --symlinks -r image-$GENIUX_VERSION-$machine.zip $machine
+            echo $sudoPW | sudo -S mkdir -p $STORE_PATH/$GENIUX_VERSION/$GENIUX_MANIFEST_DATE/images/$machine
+            echo $sudoPW | sudo -S cp image-$GENIUX_VERSION-$machine.zip $STORE_PATH/$GENIUX_VERSION/$GENIUX_MANIFEST_DATE/images/$machine
+            cd ..
+            echo $sudoPW | sudo -S mkdir -p $STORE_PATH/$GENIUX_VERSION/$GENIUX_MANIFEST_DATE/sdk
+            echo $sudoPW | sudo -S cp $PWD/sdk/* $STORE_PATH/$GENIUX_VERSION/$GENIUX_MANIFEST_DATE/sdk
+            cd ../..
+        else
+          	if [ $STORE_PATH ]
+	            then
+					zip --symlinks -r image-$GENIUX_VERSION-$machine.zip $machine
+					mkdir -p $STORE_PATH/$GENIUX_VERSION/$GENIUX_MANIFEST_DATE/images/$machine
+					cp image-$GENIUX_VERSION-$machine.zip $STORE_PATH/$GENIUX_VERSION/$GENIUX_MANIFEST_DATE/images/$machine
+					cd ..
+					mkdir -p $STORE_PATH/$GENIUX_VERSION/$GENIUX_MANIFEST_DATE/sdk
+					cp $PWD/sdk/* $STORE_PATH/$GENIUX_VERSION/$GENIUX_MANIFEST_DATE/sdk
+					cd ../..
+	    	    else
+			        # Do not store products, just leave them at $GENIUX_VERSION/output_$machine
+				    cd $BASEDIR/$GENIUX_VERSION
+            fi
+    fi
+done
+
+cd $BASEDIR
